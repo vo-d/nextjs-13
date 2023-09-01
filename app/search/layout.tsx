ab39d4af7fb6cdbb94a2e37bfeb04db8d2fe124d
vo-d <daidaivo2002@gmail.com>
2023-09-01 17:45:39 -0500

    head

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,4 +1,5 @@
 import Search from "./Search";
+import Head from "./head";
 
 export default function RootLayout({
     children,
@@ -6,14 +7,18 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <main className="flex space-x-4 divide-x-2 p-5">
-            <div>
-                <h1>Search</h1>
-            </div>
-            <div className="flex-1 pl-5">
-                <Search></Search>
-                <div>{children}</div>
-            </div>
-        </main>
+        <>
+            <Head></Head>
+            <main className="flex space-x-4 divide-x-2 p-5">
+                <div>
+                    <h1>Search</h1>
+                </div>
+                <div className="flex-1 pl-5">
+                    <Search></Search>
+                    <div>{children}</div>
+                </div>
+            </main>
+        </>
+       
     );
 }
